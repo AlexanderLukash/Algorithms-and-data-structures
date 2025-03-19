7715f8de9c4d048d46237b054b1c1a07279991f6
AlexanderLukash <sashass20062727@gmail.com>
2025-03-19 12:53:36 +0200

    Heap Sort, Shell Sort

--- Heap Sort/main.py
+++ Heap Sort/main.py
@@ -1,6 +1,33 @@
+def heapify(nums: list[int], length: int, index: int):
+    largest = index
+
+    left_index = 2 * index + 1
+    right_index = 2 * index + 2
+
+    if left_index < length and nums[left_index] > nums[largest]:
+        largest = left_index
+
+    if right_index < length and nums[right_index] > nums[largest]:
+        largest = right_index
+
+    if largest != index:
+        nums[index], nums[largest] = nums[largest], nums[index]
+        heapify(nums, length, index)
+
+
 def HeapSort(nums: list[int]) -> list[int]:
-    ...
+    length = len(nums)
+
+    for i in range(length // 2 - 1, -1, -1):
+        heapify(nums, length, i)
+
+    for i in range(length - 1, 0, -1):
+        nums[0], nums[i] = nums[i], nums[0]
+        heapify(nums, i, 0)
+
+    return nums
 
 
 if __name__ == '__main__':
     print(HeapSort([5, 3, 8, 8, 4, 1]))
+
---
+++ Shell Sort/main.py
@@ -0,0 +1,21 @@
+def ShellSort(nums: list[int]) -> list[int]:
+    length = len(nums)
+    gap = length // 2
+
+    while gap > 0:
+        for i in range(gap, length):
+            temp = nums[i]
+            j = i
+            while j >= gap and nums[j - gap] > temp:
+                nums[j] = nums[j - gap]
+                j -= gap
+            nums[j] = temp
+
+        gap //= 2
+
+    return nums
+
+
+
+if __name__ == '__main__':
+    print(ShellSort([5, 3, 8, 8, 4, 1]))
